7b775d7aae18fbfde195a82a479a13128c8dfc8a
Michaël <sagotmickael@neuf.fr>
2018-02-27 19:31:09 +0100

    Version initiale

---
+++ app/src/main/java/com/ponroy/florian/topquiz/model/Scores.java
@@ -0,0 +1,37 @@
+package com.ponroy.florian.topquiz.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Michaël SAGOT on 22/02/2018.
+ */
+
+public class Scores implements Serializable{
+
+   private List<User> mScoresTab;
+
+    public Scores() {
+
+        mScoresTab = new ArrayList<User>();
+    }
+
+    public Scores(ArrayList<User> scoresTab) {
+        mScoresTab = scoresTab;
+    }
+
+    public List<User> getScoresTab() {
+        return mScoresTab;
+    }
+
+    public void setScoresTab(List<User> scoresTab) {
+        mScoresTab = scoresTab;
+    }
+
+    // Addition a new score
+    public void addScore(User user){
+        mScoresTab.add(user);
+    }
+}
